0544773656c9abeef37778cfd1b6e2220f5b333a
Dileep Thiriveedhi <dileep2424@gmail.com>
2022-10-15 19:12:00 +0530

    basic chat app

---
+++ app.js
@@ -0,0 +1,38 @@
+const express=require('express');
+
+const fs=require('fs');
+
+const { urlencoded } = require('body-parser');
+
+const app=express();
+
+app.use(urlencoded({extended:false}));
+
+app.get('/login',(req,res,next)=>{
+    res.send(`<form action="/" 
+    onsubmit="localStorage.setItem('Username',document.getElementById('Username').value)" 
+    method="POST"><label for="user">Username: </label>
+    <input type="text" id="Username" name="Username"><br><button type="submit">Log In</button></form>`);
+    
+})
+
+app.get('/',(req,res,next)=>{
+   res.send(`<form action="/" 
+   onsubmit="document.getElementById('Username').value=localStorage.getItem('Username')" 
+   method="post">
+   <input type="text" id="message" name="message">
+   <input type="hidden" id="Username" name="Username">
+   <button type="submit">Send</button></form>`);
+    
+})
+
+app.post('/',(req,res,next)=>{
+    // console.log(req.body);
+    fs.writeFile('username.txt',`${req.body.Username}:${req.body.message} `,(err)=>{
+        if(err){
+            console.log(err);
+        }
+    });
+    res.redirect('/'); 
+})
+app.listen(3000);
